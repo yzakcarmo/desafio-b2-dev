bf02453efa0b4240fe4b55d070f3f8fc95b19269
Yzak Carmo <yzak.carmo@gmail.com>
2026-05-06 19:52:20 -0300

    feat: Implementando o Strategy Pattern

---
+++ backend/src/main/java/com/yzakcarmo/desafiob2dev/domain/enums/OrderOrigin.java
@@ -0,0 +1,8 @@
+package com.yzakcarmo.desafiob2dev.domain.enums;
+
+public enum OrderOrigin {
+    API,
+    MOBILE,
+    SYNC,
+    BONUS
+}
---
+++ backend/src/main/java/com/yzakcarmo/desafiob2dev/domain/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.yzakcarmo.desafiob2dev.domain.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
